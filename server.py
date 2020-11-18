baa3485de8be3af8f97cf7bf779385fa9faab384
Anonymous <>
2020-11-18 12:50:06 +1100

    Artists: Search with basic database integration Missing artist icon functionality. Python's psycopg2 doesn't return named indexes for rows (by default), which makes the code less self-documenting. The pagination query manipulation needs fixing, so it's commented for now.

--- server.py
+++ server.py
@@ -1,17 +1,44 @@
 from flask import Flask, render_template, request, redirect, url_for
 from markupsafe import Markup
+import psycopg2
 app = Flask(
     __name__,
     template_folder='views'
 )
+try:
+    connection = psycopg2.connect(
+        host = 'localhost',
+        dbname = 'kemonodb',
+        user = 'nano',
+        password = 'shinonome'
+    )
+    cursor = connection.cursor()
+except Exception as error:
+    print("Failed to connect to the database: ",error)
 
 @app.route('/')
 def artists():
     props = {
         'currentPage': 'artists'
     }
-    results = {}
-    return render_template('artists.html', props=props)
+    if not request.args.get('commit'):
+        results = {}
+    else:
+        query = "SELECT * FROM lookup "
+        query += "WHERE name ILIKE '%{}%' ".format(request.args.get('q'))
+        if request.args.get('service'):
+            query += "AND service = '{}' ".format(request.args.get('service'))
+        query += "AND service != 'discord-channel' "
+        query += "ORDER BY {0} {1} ".format(request.args.get('sort_by'), request.args.get('order'))
+        query += "OFFSET {} ".format(request.args.get('o') if request.args.get('o') else 0)
+        query += "LIMIT 25"
+        cursor.execute(query)
+        results = cursor.fetchall()
+    return render_template(
+        'artists.html',
+        props = props,
+        results = results
+    )
 
 @app.route('/artists')
 def root():
