45a95685eed441784cad487f6d9b0117d5390116
Anonymous <>
2021-02-03 23:14:24 -0500

    Remove proxy

--- server.py
+++ server.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 load_dotenv(join(dirname(__file__), '.env'))
 
 from routes.help import help_app
-from routes.proxy import proxy_app
 
 from PIL import Image
 from flask import Flask, jsonify, render_template, render_template_string, request, redirect, url_for, send_from_directory, make_response, g, abort, current_app, send_file
@@ -31,7 +30,6 @@ app.url_map.strict_slashes = False
 app.jinja_env.filters['regex_match'] = lambda val, rgx: re.search(rgx, val)
 app.jinja_env.filters['regex_find'] = lambda val, rgx: re.findall(rgx, val)
 app.register_blueprint(help_app, url_prefix='/help')
-app.register_blueprint(proxy_app, url_prefix='/proxy')
 try:
     pool = psycopg2.pool.SimpleConnectionPool(1, 20,
         host = getenv('PGHOST') if getenv('PGHOST') else 'localhost',
