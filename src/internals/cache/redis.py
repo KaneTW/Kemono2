51db753c1163c38f0821d74ff28dd3e45bf7de98
RandomSuperUser <nope1029384756@gmail.com>
2021-03-03 23:08:21 -0500

    change redis to work similarly to database pool

--- src/internals/cache/redis.py
+++ src/internals/cache/redis.py
@@ -2,8 +2,12 @@ import redis
 from flask import current_app
 from os import getenv
 
-def make_pool():
-    return redis.ConnectionPool(host=getenv('REDIS_HOST'), port=getenv('REDIS_PORT'))
+pool = None
+
+def init():
+    global pool
+    pool = redis.ConnectionPool(host=getenv('REDIS_HOST'), port=getenv('REDIS_PORT'))
+    return pool
 
 def get_conn():
-    return redis.Redis(connection_pool=current_app.config['REDIS_POOL'])
+    return redis.Redis(connection_pool=pool)
