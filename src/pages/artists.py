743b6f989890566778e4826af6d35770ccc772a6
Anonymous <>
2021-03-12 08:21:52 -0500

    Change endpoint names (again)

--- src/pages/artists.py
+++ src/pages/artists.py
@@ -44,7 +44,7 @@ def list():
     return response
 
 @artists.route('/<service>/user/<id>')
-def get(service, id):
+def artist_page(service, id):
     cursor = get_cursor()
     props = {
         'currentPage': 'posts',
