73e44a4bce1c4727ebfd15d91d9e08f1684da9cd
Anonymous <>
2021-03-31 07:22:00 -0400

    Fix favoriting

--- static/js/user.js
+++ static/js/user.js
@@ -1,9 +1,3 @@
-require.config({
-  paths: {
-    oboe: 'https://unpkg.com/oboe@2.1.5/dist/oboe-browser.min'
-  }
-});
-
 /* eslint-disable no-unused-vars */
 function favorite_artist(service, user) {
   fetch(`/favorites/artist/${service}/${user}`, {
@@ -33,25 +27,3 @@ function unfavorite_artist(service, user) {
   });
 }
 /* eslint-enable no-unused-vars */
-
-function loadQuery () {
-  const query = document.getElementById('search-input').value;
-  const pathname = window.location.pathname.split('/');
-  const contentView = document.getElementById('content');
-  contentView.innerHTML = '';
-  const api = ({
-    patreon: `/api/patreon/user/${pathname[3]}/lookup?q=${query}`,
-    fanbox: `/api/fanbox/user/${pathname[3]}/lookup?q=${query}`,
-    gumroad: `/api/gumroad/user/${pathname[3]}/lookup?q=${query}`,
-    subscribestar: `/api/subscribestar/user/${pathname[3]}/lookup?q=${query}`,
-    dlsite: `/api/dlsite/user/${pathname[3]}/lookup?q=${query}`
-  })[document.getElementsByName('service')[0].content];
-  require(['oboe'], function (oboe) {
-    oboe(api)
-      .node('!.*', function (post) {
-        return renderPost(post);
-      });
-  });
-}
-
-document.getElementById('search-input').addEventListener('keyup', debounce(() => loadQuery(), 350));
