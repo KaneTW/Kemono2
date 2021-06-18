3f7801d83d40b70965430b980e5b0a120d8a7d2c
Anonymous <>
2021-06-17 21:18:46 -0400

    Update account.py

--- src/pages/account.py
+++ src/pages/account.py
@@ -19,7 +19,7 @@ def get_login():
 
     account = load_account()
     if account is not None:
-        return redirect(url_for('artists.list'))
+        return redirect(set_query_parameter(url_for('artists.list'), 'logged_in', 'yes'))
 
     query = request.query_string.decode('utf-8')
     if len(query) > 0:
