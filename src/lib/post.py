52b986b25738c6b00c63afd0f146857b8f7ba882
Anonymous <>
2021-11-23 22:23:40 -0500

    perf: prevent concurrent library calls by using locks

--- src/lib/post.py
+++ src/lib/post.py
@@ -1,140 +1,181 @@
 from ..internals.cache.redis import get_conn
 from ..internals.database.database import get_cursor
 import ujson
+import redis_lock
 import dateutil
 import datetime
 import copy
 import re
 
-def get_random_posts_keys(count, reload = False):
+
+def get_random_posts_keys(count, reload=False):
     redis = get_conn()
     key = 'random_post_keys:' + str(count)
     post_keys = redis.get(key)
     if post_keys is None or reload:
-        cursor = get_cursor()
-        query = "SELECT id, \"user\", service FROM posts WHERE file != '{}' AND attachments != '{}' ORDER BY random() LIMIT %s"
-        cursor.execute(query, (count,))
-        post_keys = cursor.fetchall()
-        redis.set(key, ujson.dumps(post_keys), ex = 600)
+        lock = redis_lock.Lock(redis, key, expire=60, auto_renewal=True)
+        if lock.acquire(blocking=False):
+            cursor = get_cursor()
+            query = "SELECT id, \"user\", service FROM posts WHERE file != '{}' AND attachments != '{}' ORDER BY random() LIMIT %s"
+            cursor.execute(query, (count,))
+            post_keys = cursor.fetchall()
+            redis.set(key, ujson.dumps(post_keys), ex=600)
+        else:
+            return get_random_posts_keys(count, reload=reload)
     else:
         post_keys = ujson.loads(post_keys)
     return post_keys
 
-def get_all_post_keys(reload = False):
+
+def get_all_post_keys(reload=False):
     redis = get_conn()
     key = 'all_post_keys'
     post_keys = redis.get(key)
     if post_keys is None or reload:
-        cursor = get_cursor()
-        query = "SELECT id, \"user\", service FROM posts"
-        cursor.execute(query)
-        post_keys = cursor.fetchall()
-        redis.set(key, ujson.dumps(post_keys), ex = 600)
+        lock = redis_lock.Lock(redis, key, expire=60, auto_renewal=True)
+        if lock.acquire(blocking=False):
+            cursor = get_cursor()
+            query = "SELECT id, \"user\", service FROM posts"
+            cursor.execute(query)
+            post_keys = cursor.fetchall()
+            redis.set(key, ujson.dumps(post_keys), ex=600)
+        else:
+            return get_all_post_keys(reload=reload)
     else:
         post_keys = ujson.loads(post_keys)
     return post_keys
 
-def get_post(post_id, artist_id, service, reload = False):
+
+def get_post(post_id, artist_id, service, reload=False):
     redis = get_conn()
     key = 'post:' + service + ':' + str(artist_id) + ':' + str(post_id)
     post = redis.get(key)
     if post is None or reload:
-        cursor = get_cursor()
-        query = 'SELECT * FROM posts WHERE id = %s AND \"user\" = %s AND service = %s'
-        cursor.execute(query, (post_id, artist_id, service))
-        post = cursor.fetchone()
-        redis.set(key, serialize_post(post), ex = 600)
+        lock = redis_lock.Lock(redis, key, expire=60, auto_renewal=True)
+        if lock.acquire(blocking=False):
+            cursor = get_cursor()
+            query = 'SELECT * FROM posts WHERE id = %s AND \"user\" = %s AND service = %s'
+            cursor.execute(query, (post_id, artist_id, service))
+            post = cursor.fetchone()
+            redis.set(key, serialize_post(post), ex=600)
+        else:
+            return get_post(post_id, artist_id, service, reload=reload)
     else:
         post = deserialize_post(post)
     return post
 
-def get_post_comments(post_id, service, reload = False):
+
+def get_post_comments(post_id, service, reload=False):
     redis = get_conn()
     key = 'comments:' + service + ':' + str(post_id)
     comments = redis.get(key)
     if comments is None or reload:
-        cursor = get_cursor()
-        query = 'SELECT * FROM comments WHERE post_id = %s AND service = %s'
-        cursor.execute(query, (post_id, service))
-        comments = cursor.fetchall()
-        redis.set(key, serialize_comments(comments), ex = 600)
+        lock = redis_lock.Lock(redis, key, expire=60, auto_renewal=True)
+        if lock.acquire(blocking=False):
+            cursor = get_cursor()
+            query = 'SELECT * FROM comments WHERE post_id = %s AND service = %s'
+            cursor.execute(query, (post_id, service))
+            comments = cursor.fetchall()
+            redis.set(key, serialize_comments(comments), ex=600)
+        else:
+            return get_post_comments(post_id, service, reload=reload)
     else:
         comments = deserialize_comments(comments)
     return comments
 
-def get_all_posts_by_artist(artist_id, service, reload = False):
+
+def get_all_posts_by_artist(artist_id, service, reload=False):
     redis = get_conn()
     key = 'posts_by_artist:' + service + ':' + str(artist_id)
     posts = redis.get(key)
     if posts is None or reload:
-        cursor = get_cursor()
-        query = 'SELECT * FROM posts WHERE \"user\" = %s AND service = %s'
-        cursor.execute(query, (artist_id, service))
-        posts = cursor.fetchall()
-        redis.set(key, serialize_posts(posts), ex = 600)
+        lock = redis_lock.Lock(redis, key, expire=60, auto_renewal=True)
+        if lock.acquire(blocking=False):
+            cursor = get_cursor()
+            query = 'SELECT * FROM posts WHERE \"user\" = %s AND service = %s'
+            cursor.execute(query, (artist_id, service))
+            posts = cursor.fetchall()
+            redis.set(key, serialize_posts(posts), ex=600)
+        else:
+            return get_all_posts_by_artist(artist_id, service, reload=reload)
     else:
         posts = deserialize_posts(posts)
     return posts
 
-def get_artist_posts(artist_id, service, offset, limit, sort = 'id', reload = False):
+
+def get_artist_posts(artist_id, service, offset, limit, sort='id', reload=False):
     redis = get_conn()
     key = 'artist_posts_offset:' + service + ':' + str(artist_id) + ':' + str(offset)
     posts = redis.get(key)
     if posts is None or reload:
-        cursor = get_cursor()
-        query = 'SELECT * FROM posts WHERE \"user\" = %s AND service = %s ORDER BY ' + sort + ' OFFSET %s LIMIT %s'
-        cursor.execute(query, (artist_id, service, offset, limit,))
-        posts = cursor.fetchall()
-        redis.set(key, serialize_posts(posts), ex = 600)
+        lock = redis_lock.Lock(redis, key, expire=60, auto_renewal=True)
+        if lock.acquire(blocking=False):
+            cursor = get_cursor()
+            query = 'SELECT * FROM posts WHERE \"user\" = %s AND service = %s ORDER BY ' + sort + ' OFFSET %s LIMIT %s'
+            cursor.execute(query, (artist_id, service, offset, limit,))
+            posts = cursor.fetchall()
+            redis.set(key, serialize_posts(posts), ex=600)
+        else:
+            return get_artist_posts(artist_id, service, offset, limit, sort=sort, reload=reload)
     else:
         posts = deserialize_posts(posts)
     return posts
 
-def is_post_flagged(service, artist_id, post_id, reload = False):
+
+def is_post_flagged(service, artist_id, post_id, reload=False):
     redis = get_conn()
     key = 'is_post_flagged:' + service + ':' + str(artist_id) + ':' + str(post_id)
     flagged = redis.get(key)
     if flagged is None or reload:
-        cursor = get_cursor()
-        query = "SELECT * FROM booru_flags WHERE id = %s AND \"user\" = %s AND service = %s"
-        cursor.execute(query, (post_id, artist_id, service,))
-        flagged = cursor.fetchone() is not None
-        redis.set(key, str(flagged), ex = 600)
+        lock = redis_lock.Lock(redis, key, expire=60, auto_renewal=True)
+        if lock.acquire(blocking=False):
+            cursor = get_cursor()
+            query = "SELECT * FROM booru_flags WHERE id = %s AND \"user\" = %s AND service = %s"
+            cursor.execute(query, (post_id, artist_id, service,))
+            flagged = cursor.fetchone() is not None
+            redis.set(key, str(flagged), ex=600)
+        else:
+            return is_post_flagged(service, artist_id, post_id, reload=reload)
     else:
         flagged = flagged.decode('utf-8') == 'True'
     return flagged
 
-def get_next_post_id(post_id, artist_id, service, reload = False):
+
+def get_next_post_id(post_id, artist_id, service, reload=False):
     redis = get_conn()
     key = 'next_post:' + service + ':' + str(artist_id) + ':' + str(post_id)
     next_post = redis.get(key)
     if next_post is None or reload:
-        cursor = get_cursor()
-        query = """
-            SELECT id
-            FROM posts
-            WHERE
-                posts.user = %s
-                AND service = %s
-                AND published < (
-                    SELECT published
-                    FROM posts
-                    WHERE
-                        id = %s
-                        AND "user" = %s
-                        AND service = %s
-                    LIMIT 1
-                )
-            ORDER BY published DESC
-            LIMIT 1
-        """
-        cursor.execute(query, (artist_id, service, post_id, artist_id, service))
-        next_post = cursor.fetchone()
-        if next_post is None:
-            next_post = ""
+        lock = redis_lock.Lock(redis, key, expire=60, auto_renewal=True)
+        if lock.acquire(blocking=False):
+            cursor = get_cursor()
+            query = """
+                SELECT id
+                FROM posts
+                WHERE
+                    posts.user = %s
+                    AND service = %s
+                    AND published < (
+                        SELECT published
+                        FROM posts
+                        WHERE
+                            id = %s
+                            AND "user" = %s
+                            AND service = %s
+                        LIMIT 1
+                    )
+                ORDER BY published DESC
+                LIMIT 1
+            """
+            cursor.execute(query, (artist_id, service, post_id, artist_id, service))
+            next_post = cursor.fetchone()
+            if next_post is None:
+                next_post = ""
+            else:
+                next_post = next_post['id']
+            redis.set(key, str(next_post), ex=600)
         else:
-            next_post = next_post['id']
-        redis.set(key, str(next_post), ex = 600)
+            return get_next_post_id(post_id, artist_id, service, reload=reload)
     else:
         next_post = next_post.decode('utf-8')
 
@@ -143,37 +184,42 @@ def get_next_post_id(post_id, artist_id, service, reload = False):
     else:
         return next_post
 
-def get_previous_post_id(post_id, artist_id, service, reload = False):
+
+def get_previous_post_id(post_id, artist_id, service, reload=False):
     redis = get_conn()
     key = 'previous_post:' + service + ':' + str(artist_id) + ':' + str(post_id)
     prev_post = redis.get(key)
     if prev_post is None or reload:
-        cursor = get_cursor()
-        query = """
-            SELECT id
-            FROM posts
-            WHERE
-                posts.user = %s
-                AND service = %s
-                AND published > (
-                    SELECT published
-                    FROM posts
-                    WHERE
-                        id = %s
-                        AND "user" = %s
-                        AND service = %s
-                    LIMIT 1
-                )
-            ORDER BY published ASC
-            LIMIT 1
-        """
-        cursor.execute(query, (artist_id, service, post_id, artist_id, service,))
-        prev_post = cursor.fetchone()
-        if prev_post is None:
-            prev_post = ""
+        lock = redis_lock.Lock(redis, key, expire=60, auto_renewal=True)
+        if lock.acquire(blocking=False):
+            cursor = get_cursor()
+            query = """
+                SELECT id
+                FROM posts
+                WHERE
+                    posts.user = %s
+                    AND service = %s
+                    AND published > (
+                        SELECT published
+                        FROM posts
+                        WHERE
+                            id = %s
+                            AND "user" = %s
+                            AND service = %s
+                        LIMIT 1
+                    )
+                ORDER BY published ASC
+                LIMIT 1
+            """
+            cursor.execute(query, (artist_id, service, post_id, artist_id, service,))
+            prev_post = cursor.fetchone()
+            if prev_post is None:
+                prev_post = ""
+            else:
+                prev_post = prev_post['id']
+            redis.set(key, str(prev_post), ex=600)
         else:
-            prev_post = prev_post['id']
-        redis.set(key, str(prev_post), ex = 600)
+            return get_previous_post_id(post_id, artist_id, service, reload=reload)
     else:
         prev_post = prev_post.decode('utf-8')
 
@@ -181,6 +227,7 @@ def get_previous_post_id(post_id, artist_id, service, reload = False):
         return None
     else:
         return prev_post
+
 
 def get_render_data_for_posts(posts):
     result_previews = []
@@ -198,17 +245,17 @@ def get_render_data_for_posts(posts):
         previews = []
         attachments = []
         if len(post['file']):
-            if re.search("\.(gif|jpe?g|jpe|png|webp)$", post['file']['path'], re.IGNORECASE):
+            if re.search("\.(gif|jpe?g|jpe|png|webp)$", post['file']['path'], re.IGNORECASE):  # noqa: W605
                 result_is_image.append(True)
                 previews.append({
                     'type': 'thumbnail',
                     'name': post['file'].get('name'),
-                    'path': post['file']['path'].replace('https://kemono.party','')
+                    'path': post['file']['path'].replace('https://kemono.party', '')
                 })
             else:
                 result_is_image.append(False)
                 attachments.append({
-                    'path': post['file']['path'].replace('https://kemono.party',''),
+                    'path': post['file']['path'].replace('https://kemono.party', ''),
                     'name': post['file'].get('name')
                 })
         else:
@@ -222,11 +269,11 @@ def get_render_data_for_posts(posts):
                 'description': post['embed']['description']
             })
         for attachment in post['attachments']:
-            if re.search("\.(gif|jpe?g|jpe|png|webp)$", attachment['path'], re.IGNORECASE):
+            if re.search("\.(gif|jpe?g|jpe|png|webp)$", attachment['path'], re.IGNORECASE):  # noqa: W605
                 previews.append({
                     'type': 'thumbnail',
                     'name': attachment.get('name'),
-                    'path': attachment['path'].replace('https://kemono.party','')
+                    'path': attachment['path'].replace('https://kemono.party', '')
                 })
             else:
                 attachments.append({
@@ -245,22 +292,27 @@ def serialize_posts(posts):
     posts = copy.deepcopy(posts)
     return ujson.dumps(list(map(lambda post: prepare_post_fields(post), posts)))
 
+
 def deserialize_posts(posts_str):
     posts = ujson.loads(posts_str)
     return list(map(lambda post: rebuild_post_fields(post), posts))
+
 
 def serialize_comments(comments):
     comments = copy.deepcopy(comments)
     return ujson.dumps(list(map(lambda post: prepare_comment_fields(post), comments)))
 
+
 def deserialize_comments(comments_str):
     comments = ujson.loads(comments_str)
     return list(map(lambda post: rebuild_comment_fields(post), comments))
+
 
 def serialize_post(post):
     if post is not None:
         post = prepare_post_fields(copy.deepcopy(post))
     return ujson.dumps(post)
+
 
 def deserialize_post(post_str):
     post = ujson.loads(post_str)
@@ -268,11 +320,13 @@ def deserialize_post(post_str):
         post = rebuild_post_fields(post)
     return post
 
+
 def prepare_post_fields(post):
     post['added'] = post['added'].isoformat()
     post['published'] = post['published'].isoformat() if post['published'] else None
     post['edited'] = post['edited'].isoformat() if post['edited'] else None
     return post
+
 
 def rebuild_post_fields(post):
     post['added'] = dateutil.parser.parse(post['added'])
@@ -280,10 +334,12 @@ def rebuild_post_fields(post):
     post['edited'] = dateutil.parser.parse(post['edited']) if post['edited'] else None
     return post
 
+
 def prepare_comment_fields(comment):
     comment['added'] = comment['added'].isoformat()
     comment['published'] = comment['published'].isoformat() if comment['published'] else None
     return comment
+
 
 def rebuild_comment_fields(comment):
     comment['added'] = dateutil.parser.parse(comment['added'])
--- src/lib/posts.py
+++ src/lib/posts.py
@@ -6,6 +6,7 @@ import ujson
 import dateutil
 import copy
 import datetime
+import redis_lock
 
 
 def count_all_posts(reload=False):
@@ -13,12 +14,16 @@ def count_all_posts(reload=False):
     key = 'global_post_count'
     count = redis.get(key)
     if count is None or reload:
-        cursor = get_cursor()
-        query = 'SELECT COUNT(*) FROM posts'
-        cursor.execute(query)
-        count = cursor.fetchone()
-        redis.set(key, str(count['count']), ex=600)
-        count = int(count['count'])
+        lock = redis_lock.Lock(redis, key, expire=60, auto_renewal=True)
+        if lock.acquire(blocking=False):
+            cursor = get_cursor()
+            query = 'SELECT COUNT(*) FROM posts'
+            cursor.execute(query)
+            count = cursor.fetchone()
+            redis.set(key, str(count['count']), ex=600)
+            count = int(count['count'])
+        else:
+            return count_all_posts(reload=reload)
     else:
         count = int(count)
     return count
@@ -29,12 +34,16 @@ def count_all_posts_for_query(q: str, reload=False):
     key = 'global_post_count_for_query:' + q
     count = redis.get(key)
     if count is None or reload:
-        cursor = get_cursor()
-        query = "SELECT COUNT(*) FROM posts WHERE to_tsvector('english', content || ' ' || title) @@ websearch_to_tsquery(%s)"
-        cursor.execute(query, (q,))
-        count = cursor.fetchone()
-        redis.set(key, str(count['count']), ex=600)
-        count = int(count['count'])
+        lock = redis_lock.Lock(redis, key, expire=60, auto_renewal=True)
+        if lock.acquire(blocking=False):
+            cursor = get_cursor()
+            query = "SELECT COUNT(*) FROM posts WHERE to_tsvector('english', content || ' ' || title) @@ websearch_to_tsquery(%s)"
+            cursor.execute(query, (q,))
+            count = cursor.fetchone()
+            redis.set(key, str(count['count']), ex=600)
+            count = int(count['count'])
+        else:
+            return count_all_posts_for_query(q, reload=reload)
     else:
         count = int(count)
     return count
@@ -45,11 +54,15 @@ def get_all_posts(offset: int, reload=False):
     key = 'all_posts:' + str(offset)
     all_posts = redis.get(key)
     if all_posts is None or reload:
-        cursor = get_cursor()
-        query = 'SELECT * FROM posts ORDER BY added desc OFFSET %s LIMIT 25'
-        cursor.execute(query, (offset,))
-        all_posts = cursor.fetchall()
-        redis.set(key, serialize_dict_list(all_posts), ex=600)
+        lock = redis_lock.Lock(redis, key, expire=60, auto_renewal=True)
+        if lock.acquire(blocking=False):
+            cursor = get_cursor()
+            query = 'SELECT * FROM posts ORDER BY added desc OFFSET %s LIMIT 25'
+            cursor.execute(query, (offset,))
+            all_posts = cursor.fetchall()
+            redis.set(key, serialize_dict_list(all_posts), ex=600)
+        else:
+            return get_all_posts(offset, reload=reload)
     else:
         all_posts = deserialize_dict_list(all_posts)
     return all_posts
@@ -60,14 +73,18 @@ def get_all_posts_for_query(q: str, offset: int, reload=False):
     key = 'all_posts_for_query:' + q + ':' + str(offset)
     results = redis.get(key)
     if results is None or reload:
-        cursor = get_cursor()
-        query = "SET LOCAL enable_indexscan = off; "
-        query += "SELECT * FROM posts WHERE to_tsvector('english', content || ' ' || title) @@ websearch_to_tsquery(%s) ORDER BY added desc LIMIT 25 OFFSET %s"
-        params = (q, offset)
+        lock = redis_lock.Lock(redis, key, expire=60, auto_renewal=True)
+        if lock.acquire(blocking=False):
+            cursor = get_cursor()
+            query = "SET LOCAL enable_indexscan = off; "
+            query += "SELECT * FROM posts WHERE to_tsvector('english', content || ' ' || title) @@ websearch_to_tsquery(%s) ORDER BY added desc LIMIT 25 OFFSET %s"
+            params = (q, offset)
 
-        cursor.execute(query, params)
-        results = cursor.fetchall()
-        redis.set(key, serialize_dict_list(results), ex=600)
+            cursor.execute(query, params)
+            results = cursor.fetchall()
+            redis.set(key, serialize_dict_list(results), ex=600)
+        else:
+            return get_all_posts_for_query(q, offset, reload=reload)
     else:
         results = deserialize_dict_list(results)
     return results
