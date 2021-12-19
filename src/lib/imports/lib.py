d8e0b53704e6cf6e1bed5c79f21da13ed13808e3
BassOfBass <vinniethepooloo@protonmail.com>
2021-12-19 20:20:42 +0500

    chore(server): better validation

--- src/lib/imports/lib.py
+++ src/lib/imports/lib.py
@@ -1,6 +1,6 @@
 from typing import Union
 from src.types.kemono_error import KemonoError, KemonoValidationError
-from .types import service_constraints
+from .types import service_constraints, Encodings
 
 
 def validate_import_key(key: str, service: str) -> Union[KemonoError, str]:
@@ -8,19 +8,40 @@ def validate_import_key(key: str, service: str) -> Union[KemonoError, str]:
     Validates the key according to these rules:
     - Trim spaces from both sides.
     - Minimum and maximum length of the key (depending on service).
-    - ASCII characters only.
+    - Encoding
     """
     formatted_key = key.strip()
     key_length = len(formatted_key.encode('utf-8'))
     constraints = service_constraints[service]
-    is_valid_length = constraints.min_length < key_length < constraints.max_length
+    is_valid_length = None
 
-    # key is within length constraints
+    # is key of fixed length
+    if (constraints.length):
+        is_valid_length = constraints.length == key_length
+    else:
+        is_valid_length = constraints.min_length < key_length < constraints.max_length
+
+    # is key within length constraints
     if (not is_valid_length):
-        return KemonoValidationError(f"The key \"{key}\" of service \"{service}\" and length \"{key_length}\" is outside of \"{constraints.min_length} - {constraints.max_length}\" range. You should let the administrator know about this.")
+        req_length = constraints.length if constraints.length else f"{constraints.min_length}-{constraints.max_length}"
 
-    # key is ascii
-    if (not formatted_key.isascii()):
-        return KemonoValidationError(f"The key \"{key}\" is of invalid encoding.")
+        return KemonoValidationError(f"""
+            The key \"{key}\" of service \"{service}\" and length \"{key_length}\" is not valid length. Required length is \"{req_length}\".
+        """)
+
+    # is of valid case, if present
+    if (constraints.case == "lower" and not formatted_key.islower()):
+        return KemonoValidationError(f"""
+            The key \"{key}\" of service \"{service}\" is of invalid casing.
+        """)
+
+    # is key ascii
+    if (constraints.encoding == Encodings.ASCII and not formatted_key.isascii()):
+        return KemonoValidationError(f"The key \"{key}\" of service \"{service}\" is of invalid encoding.")
+
+    # is matching a pattern
+
+    if (constraints.pattern and not constraints.pattern.search(formatted_key)):
+        return KemonoValidationError(f"The key \"{key}\" of service \"{service}\" is of invalid pattern.")
 
     return formatted_key
--- src/lib/imports/types.py
+++ src/lib/imports/types.py
@@ -1,18 +1,37 @@
 from dataclasses import dataclass
+from re import RegexFlag, Pattern, compile as compile_regexp
+
+
+class Encodings:
+    HEX = "hex"
+    ASCII = "ascii"
 
 
 @dataclass
 class KeyConstraints:
     min_length: int = 1
     max_length: int = 1024
+    encoding: str = Encodings.ASCII
+    length: int = None
+    case: str = None
+    pattern: Pattern = None
 
 
 service_constraints = dict(
-    patreon=KeyConstraints(),
+    patreon=KeyConstraints(length=43),
     fanbox=KeyConstraints(),
-    gumroad=KeyConstraints(),
+    fantia=KeyConstraints(
+        length=32,
+        encoding=Encodings.HEX,
+        case="lower"
+    ),
+    gumroad=KeyConstraints(min_length=200),
     subscribestar=KeyConstraints(),
     dlsite=KeyConstraints(),
-    discord=KeyConstraints(),
-    fantia=KeyConstraints(),
+    discord=KeyConstraints(
+        pattern=compile_regexp(
+            pattern=r"/(mfa\.[a-z0-9_-]{20,})|([a-z0-9_-]{23,28}\.[a-z0-9_-]{6,7}\.[a-z0-9_-]{27})/i",
+            flags=RegexFlag.IGNORECASE
+        )
+    ),
 )
