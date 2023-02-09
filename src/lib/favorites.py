12da3c494b70b5313f76660b204cab001ffdf5af
Anonymous <>
2023-02-08 21:44:33 -0500

    perf: immediately commit un/favorites to prevent locking

--- src/lib/favorites.py
+++ src/lib/favorites.py
@@ -1,3 +1,5 @@
+from flask import g
+
 from ..internals.database.database import get_cursor
 from ..utils.utils import get_value
 from ..internals.cache.redis import get_conn, serialize_dict_list, deserialize_dict_list
@@ -8,7 +10,8 @@ import ujson
 import copy
 import dateutil
 
-def get_favorite_artists(account_id, reload = False):
+
+def get_favorite_artists(account_id, reload=False):
     redis = get_conn()
     key = 'favorite_artists:' + str(account_id)
     favorites = redis.get(key)
@@ -31,7 +34,8 @@ def get_favorite_artists(account_id, reload = False):
             artists.append(artist)
     return artists
 
-def get_favorite_posts(account_id, reload = False):
+
+def get_favorite_posts(account_id, reload=False):
     redis = get_conn()
     key = 'favorite_posts:' + str(account_id)
     favorites = redis.get(key)
@@ -52,7 +56,8 @@ def get_favorite_posts(account_id, reload = False):
             posts.append(post)
     return posts
 
-def is_artist_favorited(account_id, service, artist_id, reload = False):
+
+def is_artist_favorited(account_id, service, artist_id, reload=False):
     redis = get_conn()
     key = 'artist_favorited:' + str(account_id) + ':' + str(service) + str(artist_id)
     value = redis.get(key)
@@ -67,7 +72,8 @@ def is_artist_favorited(account_id, service, artist_id, reload = False):
 
     return value
 
-def is_post_favorited(account_id, service, artist_id, post_id, reload = False):
+
+def is_post_favorited(account_id, service, artist_id, post_id, reload=False):
     redis = get_conn()
     key = 'post_favorited:' + str(account_id) + ':' + str(service) + str(artist_id) + ':' + str(post_id)
     value = redis.get(key)
@@ -82,30 +88,38 @@ def is_post_favorited(account_id, service, artist_id, post_id, reload = False):
 
     return value
 
+
 def add_favorite_artist(account_id, service, artist_id):
     cursor = get_cursor()
     query = 'insert into account_artist_favorite (account_id, service, artist_id) values (%s, %s, %s) ON CONFLICT (account_id, service, artist_id) DO NOTHING'
     cursor.execute(query, (account_id, service, artist_id))
+    g.connection.commit()
     get_favorite_artists(account_id, True)
     is_artist_favorited(account_id, service, artist_id, True)
+
 
 def add_favorite_post(account_id, service, artist_id, post_id):
     cursor = get_cursor()
     query = 'insert into account_post_favorite (account_id, service, artist_id, post_id) values (%s, %s, %s, %s) ON CONFLICT (account_id, service, artist_id, post_id) DO NOTHING'
     cursor.execute(query, (account_id, service, artist_id, post_id))
+    g.connection.commit()
     get_favorite_posts(account_id, True)
     is_post_favorited(account_id, service, artist_id, post_id, True)
+
 
 def remove_favorite_artist(account_id, service, artist_id):
     cursor = get_cursor()
     query = 'delete from account_artist_favorite where account_id = %s and service = %s and artist_id = %s'
     cursor.execute(query, (account_id, service, artist_id))
+    g.connection.commit()
     get_favorite_artists(account_id, True)
     is_artist_favorited(account_id, service, artist_id, True)
+
 
 def remove_favorite_post(account_id, service, artist_id, post_id):
     cursor = get_cursor()
     query = 'delete from account_post_favorite where account_id = %s and service = %s and artist_id = %s and post_id = %s'
     cursor.execute(query, (account_id, service, artist_id, post_id))
+    g.connection.commit()
     get_favorite_posts(account_id, True)
     is_post_favorited(account_id, service, artist_id, post_id, True)
